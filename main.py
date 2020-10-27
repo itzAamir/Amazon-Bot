ec2271a3ccd30a20a2fd582c3c34b6c252ba8c67
itzAamir <69009426+itzAamir@users.noreply.github.com>
2020-10-27 10:38:27 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -28,15 +28,22 @@ def check_price(url):
         final_price = "Product is Unvailable Right Now"
         print(final_price)
 
-    emailID = os.environ["emailID"]
-    emailPass = os.environ["emailPass"]
-    receiverEmail = os.environ["receiverEmail"]
+    # emailID = os.environ["emailID"]
+    # emailPass = os.environ["emailPass"]
+    # receiverEmail = os.environ["receiverEmail"]
 
-    mail = Mailer(email= emailID, password= emailPass)
-    mail.send(receiver= receiverEmail, subject='Amazon Products Prices', message= f'Product Name: {final_product_title}\n\nProduct Price: {final_price}')
+    mail = Mailer(email= "amirpc190320@gmail.com", password= "Aamirkhan123")
+    mail.send(receiver= "amirkhan190320@gmail.com", subject='Amazon Products Prices', message= f'Product Name: {final_product_title}\n\nProduct Price: {final_price}\n\n {url}')
 
 if __name__ == "__main__":
-    while True:
+    # Below lines are for scheduling our script
+    hour = time.localtime().tm_hour
+    minutes = time.localtime().tm_min
+    seconds = time.localtime().tm_sec
+    if hour == 10 and minutes == 40 and seconds == 0:    
         url = r"https://www.amazon.in/dp/B086WN7BK6/ref=dp_prsubs_2"
         check_price(url)
-        time.sleep(60)
+    
+    if hour == 10 and minutes == 41 and seconds == 0:    
+        url = r"https://www.amazon.in/dp/B086WN7BK6/ref=dp_prsubs_2"
+        check_price(url)
